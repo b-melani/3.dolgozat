c25510b69092925ea91def6e865c1b21c99576d4
Meli <bodzamelani@gmail.com>
2021-02-08 10:40:00 +0100

    .

---
+++ dolgozat/src/main/java/com/szakaszzaro/dolgozat/DolgozatApplication.java
@@ -0,0 +1,13 @@
+package com.szakaszzaro.dolgozat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolgozatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolgozatApplication.class, args);
+	}
+
+}
---
+++ dolgozat/src/test/java/com/szakaszzaro/dolgozat/DolgozatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szakaszzaro.dolgozat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolgozatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
